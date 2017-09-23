740559312c466727785f50bcba955f9f20f13013
stzv78 <stzv78@mail.ru>
2017-09-24 01:26:48 +0400

    made adminPanel

--- resources/views/templates/question/list.blade.php
+++ resources/views/templates/question/list.blade.php
@@ -1,50 +1,63 @@
 @extends('index')
 @section('content')
-    <!-- Список вопросов -->
-    <div class="panel panel-success">
-        <div class="panel-heading lead">
-            {{ $category->name }}
-            <a class="btn btn-sm btn-success pull-right"
-               href="{{ route('question.create', ['category' => $category]) }}">Добавить вопрос </a>
-        </div>
-        <table class="table table-condensed" style="font-size: 14px;">
-            <tbody>
-            @if (isset($questions))
-                @foreach($questions as $question)
-                    <tr>
-                        <td> {{ $question->description }} </td>
-                        <td> {{ $question->created_at }} </td>
-                        @if (!($question->answer))
-                            <td>Ожидает ответа
-                                <a class="btn btn-default btn-sm" style="width: 100px" href=""> Ответить</a>
+    <div class="col-md-9">
+        <!-- Список вопросов -->
+        <ol class="breadcrumb">
+            <li><a href=" {{ route('category.index') }}"> << Назад к списку категорий</a></li>
+        </ol>
+        <div class="panel panel-success">
+            <div class="panel-heading lead">{{ $category->name }}
+                <span class="badge">Всего вопросов: {{ count($questions) }}</span>
+                <a class="btn btn-sm btn-success pull-right"
+                   href="{{ route('question.create', ['category' => $category]) }}">Добавить вопрос </a>
+            </div>
+            <table class="table table-condensed" style="font-size: 14px;">
+                <tbody>
+                @if (isset($questions))
+                    @foreach($questions as $question)
+                        <tr>
+                            <td> {{ $question->description }} </td>
+                            <td> {{ $question->created_at }} </td>
+                            @if (!$question->answer)
+                                <td>Ожидает ответа</td>
+                                <td><a class="btn btn-default btn-sm" style="width: 100px"
+                                       href="{{route('answer.create', ['question' => $question])}}"> Ответить</a></td>
+                            @elseif ($question->status === 2)
+                                <td>Опубликован</td>
+                                <td><a class="btn btn-sm btn-default" style="width: 100px" href=""> Скрыть</a></td>
+                            @elseif($question->status === 1)
+                                <td>Скрыт</td>
+                                <td><a class="btn btn-success btn-sm" style="width: 100px" href=""> Опубликовать</a>
+                                </td>
+                            @endif
+                            <td style="text-align: center">
+                                <form method="POST"
+                                      action="{{ route ('question.destroy', ['question' => $question]) }}">
+                                    <a class="btn btn-info " href=""><i
+                                                class="glyphicon glyphicon-pencil"></i></a>
+                                    <input type="hidden" name="_method" value="delete"/>
+                                    <input type="hidden" name="_token" value="{{csrf_token()}}">
+                                    <button type="submit" class="btn  btn-danger"><i
+                                                class="glyphicon glyphicon-trash"></i></button>
+                                </form>
                             </td>
-                        @elseif ($question->status === 1)
-                            <td>Опубликован
-                                <a class="btn btn-default btn-sm" style="width: 100px" href="">Скрыть</a></td>
-                        @elseif($question->status === 0)
-                            <td>Скрыт
-                                <a class="btn btn-success btn-sm" style="width: 100px" href="">Опубликовать</a></td>
-                        @endif
-                        <td style="text-align: center">
-                            <form method="POST" action="">
+                        </tr>
+                    @endforeach
+                @else
+                    <h2> Нет вопросов в категории</h2>
+                @endif
+                </tbody>
+            </table>
 
-                                <a class="btn btn-info btn-sm" href=""><i class="glyphicon glyphicon-pencil"></i></a>
-                                <input type="hidden" name="_method" value="delete"/>
-                                <input type="hidden" name="_token" value="{{csrf_token()}}">
-                                <button type="submit" class="btn btn-sm btn-danger " value=""><i
-                                            class="glyphicon glyphicon-trash"></i></button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            @else
-                <h2> Нет вопросов в категории</h2>
-            @endif
-            </tbody>
-        </table>
-        <div class="panel-success panel-footer ">
-            <h4>Всего вопросов: {{ count($questions) }}</h4>
         </div>
+        <!-- Конец списка вопросов -->
     </div>
-    <!-- Конец списка вопросов -->
+
+    <!-- Правая часть -->
+    <div class="col-md-3">
+    @include('templates.question._form', ['categories' => $categories])
+    <!-- Конец правой части -->
+    </div>
+    </div>
+    <!--Конец секции вопросов для пользователя-->
 @stop
